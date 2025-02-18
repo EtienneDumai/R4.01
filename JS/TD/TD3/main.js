15cbe10a2b5022ae9937b0133c7efe98a4b7eb7f
Etienne Dumai <etienne.dumai@gmail.com>
2025-02-18 15:15:53 +0100

    manque une seule question

--- JS/TD/TD3/main.js
+++ JS/TD/TD3/main.js
@@ -3,7 +3,7 @@ function monMax(a,b){
 }
 function calculer(a,b){
     //verifier que le type de a est int
-    if (typeof a === 'number' && typeof b === 'number') {
+    if (typeof a === 'integer' && typeof b === 'integer') {
         return a+b;
     } else {
         return "Erreur";
@@ -27,4 +27,23 @@ function max4(a,b,c,d){
 }
 function max8(a,b,c,d,e,f,g,h){
     return Math.max(a,b,c,d,e,f,g,h);
+}
+function aleatoire(a,b){
+    return Math.floor(Math.random() * (b - a + 1)) + a;
+}   
+function fctAleaGuess(){
+    var a = document.getElementById("a2").value;
+    var b = document.getElementById("b2").value;
+    parseInt(a);
+    parseInt(b);
+    var res = aleatoire(a, b);
+    var guess= document.getElementById("nbrGuess").value;
+    parseInt(guess);
+    if (guess == res){
+        console.log("Bravo");
+        alert("Bravo");
+    } else {
+        console.log("Perdu");
+        alert("Perdu");
+    }
 }
